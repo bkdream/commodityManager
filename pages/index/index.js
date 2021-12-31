e2dda0c844f4116226083c594e448fc0bb709f09
Cmyo233 <953837803@qq.com>
2022-01-01 04:08:42 +0800

    商品删除功能
    同时删除数据库数据和存储的照片

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,6 +8,7 @@ Page({
           active:0,
         })
       },
+      //主页右上角扫一扫查询功能
       onClickscanIcon(e){
         var _this = this;
         wx.scanCode({
@@ -17,12 +18,47 @@ Page({
                 commodityId:res.result,
               }).get({
                 success(res){
-                  _this.setData({
-                    commodityMessage:res.data,
-                  })
+                  if(res.data.length == 0){
+                    wx.showToast({
+                      title: '无此条商品信息',
+                      icon:'none',
+                      duration:2000
+                    });
+                    setTimeout(() => {
+                      wx.redirectTo({
+                        url: '/pages/index/index',
+                      })
+                  }, 2000);
+                  }else{
+                    _this.setData({
+                      commodityMessage:res.data,
+                    })
+                  }
+                  
                 }
               })
           }
         })
+    },
+    deleteCommodity(e){
+      var fileList = [];
+      fileList.push(e.currentTarget.dataset.commodityimg)
+      db.where({
+        commodityId:e.currentTarget.dataset.commodityid
+      }).remove({
+        success(res){
+          wx.showToast({
+            title: '删除成功',
+            icon:'success',
+            duration:2000,
+          })
+        }
+      });
+      wx.cloud.deleteFile({
+        fileList:fileList,
+        success(res){
+          console.log(res)
+        }
+      })
     }
 })
